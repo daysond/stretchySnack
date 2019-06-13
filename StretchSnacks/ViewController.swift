e61016a1c2783f2fa7e4bcc06568199941ea3a5f
Dayson Dong <daysond.yd@gmail.com>
2019-06-13 11:55:45 -0400

    Initial Commit

---
+++ StretchSnacks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StretchSnacks
+//
+//  Created by Dayson Dong on 2019-06-13.
+//  Copyright © 2019 Dayson Dong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
